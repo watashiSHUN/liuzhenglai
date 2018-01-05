197e37002d8f26fa5a0ddc83b7f978581241ef55
Richard Liu <ricl@microsoft.com>
2018-01-04 21:54:32 -0800

    update folder structure

---
+++ front-end/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Microsoft Yahei", sans-serif;
+  background: #aaa;
+}
+</style>
---
+++ front-end/src/components/Home.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="header">
+    <div class="container">
+    <div class="nav">
+    <a class="nav-item">Post</a>
+    </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style scoped lang="less">
+.header {
+  background: #fff;
+  .nav {
+    a.nav-item {
+      color: #333;
+      text-decoration: none;
+      padding: 0.5em 1em;
+      display: inline-block;
+      cursor: pointer;
+      transition: background 0.15s ease-in-out;
+
+      &:hover {
+        background: #ddd;
+      }
+    }
+  }
+}
+
+.container {
+  max-width: 1000px;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
